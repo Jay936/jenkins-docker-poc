dfc70aff9a7ac11c8289f2bd0a08e392e43d51aa
Jaydeep Bhatt <jaydeep.b@mobilelive.ca>
2020-03-22 11:22:55 -0400

    devops poc project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+
+server.get("/", (req, res) => {
+    res.json({ message: "Hello world" });
+ });
+
+ server.get("/ready", (req, res) => {
+    res.json({ message: "It is working!!!" });
+ });
+
+ const port = 3000;
+
+ server.listen(port, () => {
+     console.log(`Server listening at ${port}`);
+ });
