250fec75e43500783d672e4aaabf20316bb51850
Jaydeep Bhatt <jaydeep.b@mobilelive.ca>
2020-03-23 16:18:07 -0400

    updating nodejs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get("/", (req, res) => {
-    res.json({ message: "Hello world" });
+    res.json({ message: "Hello world - tested on March 23" });
  });
 
  server.get("/ready", (req, res) => {
